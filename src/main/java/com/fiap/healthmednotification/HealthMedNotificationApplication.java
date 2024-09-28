1dc276ac609eedb6070689f4775a953b0238c424
Diego Silveira <diego_silveira@sicredi.com.br>
2024-09-27 21:13:06 -0300

    initial commit

---
+++ src/main/java/com/fiap/healthmednotification/HealthMedNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.healthmednotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMedNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthMedNotificationApplication.class, args);
+    }
+
+}
